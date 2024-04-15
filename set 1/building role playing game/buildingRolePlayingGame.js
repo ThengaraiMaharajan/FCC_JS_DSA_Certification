92ab25a4a5a005486ae39177d9c1d0238552b2ce
ThengaraiMaharajan <tmaharajan1999@gmail.com>
2024-04-15 12:45:23 +0530

    upto step number 28 complete

---
+++ set 1/building role playing game/buildingRolePlayingGame.js
@@ -0,0 +1,11 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
